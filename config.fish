a0a9a39f7a7cc3c464eefed4ac19fa5f72818af1
Dave Menninger <dave@menninger.io>
2016-11-18 14:43:45 -0500

    fix merge conflict

--- config.fish
+++ config.fish
@@ -112,5 +112,6 @@ function mutt
 end
 
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+[ -f /usr/share/autojump/autojump.fish ]; and . /usr/share/autojump/autojump.fish
 
 set -gx TERM screen-256color-bce;
