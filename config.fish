46ce8a76a9da6a95083460c81adfc5f714f9f826
Dave Menninger <dmenninger@justworks.com>
2023-11-30 12:01:20 -0500

    tidy up

--- config.fish
+++ config.fish
@@ -1,5 +1,10 @@
 set PATH $PATH ~/bin
 
+set -gx TERM screen-256color-bce;
+
+set -g -x OKTA_TOKEN "fake"
+set -g -x OKTA_SECRET "fake"
+
 # Colors
 set normal (set_color normal)
 set magenta (set_color magenta)
@@ -18,8 +23,40 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 set __fish_git_prompt_show_informative_status 1
 
-function fish_prompt --description 'Write out the prompt'
 
+[ -f /usr/local/etc/grc.fish ]; and . /usr/local/etc/grc.fish
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+[ -f /usr/share/autojump/autojump.fish ]; and . /usr/share/autojump/autojump.fish
+[ -f ~/perl5/perlbrew/etc/perlbrew.fish ]; and . ~/perl5/perlbrew/etc/perlbrew.fish
+
+# enable history in IEx
+set -g -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+
+if which nvim >/dev/null
+  set -gx EDITOR nvim
+end
+
+# OS-specific stuff
+switch (uname)
+  case Linux
+    source {$HOME}/.asdf/asdf.fish
+  case Darwin
+    set PATH $PATH /usr/local/bin
+    set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+    [ -f /opt/homebrew/etc/grc.fish ]; and . /opt/homebrew/etc/grc.fish
+    [ -f /opt/homebrew/opt/asdf/libexec/asdf.fish ]; source /opt/homebrew/opt/asdf/libexec/asdf.fish
+    [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+    set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+  case '*'
+    echo Unknown uname
+end
+
+# ---------
+# Functions
+# ---------
+function fish_prompt --description 'Write out the prompt'
+  set -l delim '>'
   set -l last_status $status
 
   # Just calculate these once, to save a few cycles when displaying the prompt
@@ -56,25 +93,20 @@ function fish_prompt --description 'Write out the prompt'
     end
   end
 
-  set -l delim '>'
-
   switch $USER
+    case root
+      if not set -q __fish_prompt_cwd
+        if set -q fish_color_cwd_root
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+        else
+          set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+      end
 
-  case root
-
-    if not set -q __fish_prompt_cwd
-      if set -q fish_color_cwd_root
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-      else
+    case '*'
+      if not set -q __fish_prompt_cwd
         set -g __fish_prompt_cwd (set_color $fish_color_cwd)
       end
-    end
-
-  case '*'
-
-    if not set -q __fish_prompt_cwd
-      set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
 
   end
 
@@ -105,47 +137,6 @@ function fish_prompt --description 'Write out the prompt'
   printf '%s' (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
 
-[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
-[ -f /usr/share/autojump/autojump.fish ]; and . /usr/share/autojump/autojump.fish
-[ -f /usr/local/etc/grc.fish ]; and . /usr/local/etc/grc.fish
-[ -f ~/perl5/perlbrew/etc/perlbrew.fish ]; and . ~/perl5/perlbrew/etc/perlbrew.fish
-
-set -gx TERM screen-256color-bce;
-
-function vs
-  vagrant ssh;
-end
-
-
-# rbenv
-if which rbenv >/dev/null
-  set -gx PATH "$HOME/.rbenv/shims" $PATH
-  set -gx RBENV_SHELL fish
-  set RBENV_VERSION (rbenv --version | sed "s/rbenv //")
-  # echo $RBENV_VERSION
-  [ -f "/usr/local/Cellar/rbenv/$RBENV_VERSION/libexec/../completions/rbenv.fish" ]; and . "/usr/local/Cellar/rbenv/$RBENV_VERSION/libexec/../completions/rbenv.fish"
-  [ -f '/usr/lib/rbenv/completions/rbenv.fish' ]; and . '/usr/lib/rbenv/completions/rbenv.fish'
-  command rbenv rehash 2>/dev/null
-  function rbenv
-    set command $argv[1]
-    set -e argv[1]
-
-    switch "$command"
-    case rehash shell
-      source (rbenv "sh-$command" $argv|psub)
-    case '*'
-      command rbenv "$command" $argv
-    end
-  end
-end
-
-# enable history in IEx
-set -g -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
-
-# tabtab source for yo package
-# uninstall by removing these lines or running `tabtab uninstall yo`
-[ -f /usr/local/lib/node_modules/yo/node_modules/tabtab/.completions/yo.fish ]; and . /usr/local/lib/node_modules/yo/node_modules/tabtab/.completions/yo.fish
-
 function fish_greeting
   random_pokemon
   [ -f ~/Dropbox/todo/todo.md ]; and cat ~/Dropbox/todo/todo.md | sort -R | head -3;
@@ -158,87 +149,5 @@ function random_pokemon
   [ -d "$HOME/.pokesay" ]; and echo $(echo $p | string split '/' --fields 5 | string split '.' --fields 1)
 end
 
-if which nvim >/dev/null
-  set -gx EDITOR nvim
-end
-
-function ec_fish
-  $EDITOR ~/.config/fish/config.fish
-end
-
-function todo
-  set todo_path ~/Dropbox/todo/todo.md
-  switch $argv[1]
-    case ''
-      $EDITOR "$todo_path"
-    case 'add'
-      set item $argv[2..-1]
-      echo "$item" >> "$todo_path"
-    case '*'
-      set tag (string upper $argv[1])
-      echo "$tag"
-      cat "$todo_path" | grep "$tag"
-      true
-  end
-end
-
-
-function journal
-  set today (date -Idate)
-  set journal_path ~/Dropbox/todo/journal/{$today}.md
-  echo "$journal_path"
-  $EDITOR "$journal_path"
-end
-
-function journal_yaml
-  set today (date -Idate)
-  echo "---"
-  echo "date: $today"
-  echo "---"
-end
-
-function journal_recur
-  echo "stuff..."
-end
-
-function recur
-  set recur_path ~/Dropbox/todo/recur.md
-  switch $argv[1]
-    case ''
-      $EDITOR "$recur_path"
-    case 'daily'
-      set item $argv[2..-1]
-      echo "D - $item" >> "$recur_path"
-    case 'weekly'
-      set item $argv[2..-1]
-      echo "W - $item" >> "$recur_path"
-    case 'monthly'
-      set item $argv[2..-1]
-      echo "M - $item" >> "$recur_path"
-    case 'yearly'
-      set item $argv[2..-1]
-      echo "Y - $item" >> "$recur_path"
-    case '*'
-      set tag "$argv[1]"
-      echo "$tag"
-      cat "$recur_path" | grep -i "$tag"
-      true
-  end
-end
-
-# TODO OS-specific stuff
-switch (uname)
-  case Linux
-    source {$HOME}/.asdf/asdf.fish
-  case Darwin
-    set PATH $PATH /usr/local/bin
-    set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-    # source /usr/local/opt/asdf/asdf.fish
-    [ -f /opt/homebrew/opt/asdf/libexec/asdf.fish ]; source /opt/homebrew/opt/asdf/libexec/asdf.fish
-    [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
-    set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-  case '*'
-    echo Unknown uname
-end
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+# keep at the bottom
+eval "$(/opt/homebrew/bin/brew shellenv)"
