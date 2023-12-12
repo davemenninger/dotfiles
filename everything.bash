525f06cf0e3da41d59d848a23b2502e4215a3ff0
Dave Menninger <dmenninger@justworks.com>
2023-12-12 16:43:45 -0500

    add fisher

--- everything.bash
+++ everything.bash
@@ -31,5 +31,22 @@ bundle install
 echo "== rake (symlink dotfiles) =="
 rake
 
-# echo "= fish ="
-# curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+echo "= fish ="
+echo "== fisher =="
+read -p "Install fisher (curl)? (y/n)?" choice
+case "$choice" in
+  y|Y )
+    if [ -f "$HOME/.config/fish/functions/fisher.fish" ]; then
+      echo "fisher seems to be installed already"
+    else
+      echo "installing fisher:"
+      fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+      fish -c "fisher install jethrokuan/z"
+    fi
+    ;;
+  n|N ) echo "skipping fisher";;
+  * ) echo "invalid";;
+esac
+
+
+echo "= that's everything! ="
