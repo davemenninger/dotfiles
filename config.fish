e45bf215135c31c28d1e9e6d42b9c1f6f5be5ef5
Dave Menninger <dmenninger@justworks.com>
2023-12-14 13:32:42 -0500

    slim way down

--- config.fish
+++ config.fish
@@ -1,56 +1,41 @@
-set PATH $PATH ~/bin
-
 set -gx TERM screen-256color-bce;
 
-set -g -x OKTA_TOKEN "fake"
-set -g -x OKTA_SECRET "fake"
-
-# Colors
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
-
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-set __fish_git_prompt_show_informative_status 1
-
-
-[ -f /usr/local/etc/grc.fish ]; and . /usr/local/etc/grc.fish
-[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
-[ -f /usr/share/autojump/autojump.fish ]; and . /usr/share/autojump/autojump.fish
-[ -f ~/perl5/perlbrew/etc/perlbrew.fish ]; and . ~/perl5/perlbrew/etc/perlbrew.fish
-
-# enable history in IEx
-set -g -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+fish_add_path ~/bin
 
 if which nvim >/dev/null
   set -gx EDITOR nvim
 end
 
+# enable history in IEx
+set -g -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
+
 # OS-specific stuff
 switch (uname)
   case Linux
     source {$HOME}/.asdf/asdf.fish
+    [ -f /usr/local/etc/grc.fish ]; and . /usr/local/etc/grc.fish
+    [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+    [ -f /usr/share/autojump/autojump.fish ]; and . /usr/share/autojump/autojump.fish
   case Darwin
-    set PATH $PATH /usr/local/bin
-    set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
     [ -f /opt/homebrew/etc/grc.fish ]; and . /opt/homebrew/etc/grc.fish
     [ -f /opt/homebrew/opt/asdf/libexec/asdf.fish ]; source /opt/homebrew/opt/asdf/libexec/asdf.fish
     [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
-    set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+    fish_add_path /usr/local/bin
+    fish_add_path /usr/local/opt/curl/bin
+    fish_add_path /usr/local/sbin
   case '*'
     echo Unknown uname
 end
+
+# Fish git prompt
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
 
 # ---------
 # Functions
@@ -58,40 +43,6 @@ end
 function fish_prompt --description 'Write out the prompt'
   set -l delim '>'
   set -l last_status $status
-
-  # Just calculate these once, to save a few cycles when displaying the prompt
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-  end
-
-  if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-  end
-
-  if not set -q -g __fish_classic_git_functions_defined
-    set -g __fish_classic_git_functions_defined
-
-    function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-      if status --is-interactive
-        set -e __fish_prompt_user
-        commandline -f repaint ^/dev/null
-      end
-    end
-
-    function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-      if status --is-interactive
-        set -e __fish_prompt_host
-        commandline -f repaint ^/dev/null
-      end
-    end
-
-    function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-      if status --is-interactive
-        set -e __fish_prompt_status
-        commandline -f repaint ^/dev/null
-      end
-    end
-  end
 
   switch $USER
     case root
@@ -107,7 +58,10 @@ function fish_prompt --description 'Write out the prompt'
       if not set -q __fish_prompt_cwd
         set -g __fish_prompt_cwd (set_color $fish_color_cwd)
       end
+  end
 
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
   end
 
   set -l prompt_status
@@ -118,28 +72,11 @@ function fish_prompt --description 'Write out the prompt'
     set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
   end
 
-  if not set -q fish_color_user
-    set -g fish_color_user 'green'
-  end
-  if not set -q fish_color_host
-    set -g fish_color_host 'cyan'
-  end
-
-  if not set -q __fish_prompt_user
-    set -g __fish_prompt_user (set_color $fish_color_user)
-  end
-  if not set -q __fish_prompt_host
-    set -g __fish_prompt_host (set_color $fish_color_host)
-  end
-
-  printf '%s' "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd)
-  set_color normal
-  printf '%s' (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+  printf '%s' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
 
 function fish_greeting
   random_pokemon
-  [ -f ~/Dropbox/todo/todo.md ]; and cat ~/Dropbox/todo/todo.md | sort -R | head -3;
 end
 
 function random_pokemon
