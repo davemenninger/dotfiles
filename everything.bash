229cbd22132289a91d612c705d2602cae698af27
Dave Menninger <dmenninger@justworks.com>
2023-12-15 15:50:10 -0500

    check if fisher is installed before asking

--- everything.bash
+++ everything.bash
@@ -35,23 +35,25 @@ echo "== rake (symlink dotfiles) =="
 rake
 
 echo "= fish ="
-echo "== fisher =="
-read -n 1 -r -p "Install fisher (curl)? (y/n)?" choice
-case "$choice" in
-  y|Y )
-    if [ -f "$HOME/.config/fish/functions/fisher.fish" ]; then
-      echo "fisher seems to be installed already"
-    else
+echo "== install fisher =="
+if [ -f "$HOME/.config/fish/functions/fisher.fish" ]; then
+  echo "fisher seems to be installed already"
+else
+  read -n 1 -r -p "Install fisher (curl)? (y/n)?" choice
+  case "$choice" in
+    y|Y )
       echo "installing fisher:"
       fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
-      fish -c "fisher install FabioAntunes/base16-fish-shell"
-      fish -c "fisher install jethrokuan/z"
-      fish -c "fisher install jorgebucaran/getopts.fish"
-    fi
-    ;;
-  n|N ) echo "skipping fisher";;
-  * ) echo "invalid";;
-esac
+      ;;
+    n|N ) echo "skipping fisher";;
+    * ) echo "invalid";;
+  esac
+fi
+
+echo "== fisher plugins =="
+fish -c "fisher install FabioAntunes/base16-fish-shell"
+fish -c "fisher install jethrokuan/z"
+fish -c "fisher install jorgebucaran/getopts.fish"
 
 
 echo "= that's everything! ="
