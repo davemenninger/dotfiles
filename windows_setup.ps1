c8615c6c5d176b4e8da34ffbd74eb63911090a1f
Dave Menninger <dave.menninger@cincinnati-oh.gov>
2024-10-31 15:54:33 -0400

    add install command for terminal icons

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,5 +1,8 @@
 echo "installing winget stuff..."
 winget import winget_dump.json
 
+echo "installing terminal icons..."
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
 echo "installing WSL"
 wsl --install -d Ubuntu-24.04
