ebf8e7c861ee80f926fa1aae83358ff28d129df5
Dave Menninger <dave.menninger@cincinnati-oh.gov>
2024-11-22 08:26:06 -0500

    add posh-git module

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -1,8 +1,9 @@
 echo "installing winget stuff..."
 winget import winget_dump.json
 
-echo "installing terminal icons..."
+echo "installing pwsh modules..."
 Install-Module -Name Terminal-Icons -Repository PSGallery
+Install-Module -Name posh-git -Repository PSGallery
 
 echo "installing WSL"
 wsl --install -d Ubuntu-24.04
