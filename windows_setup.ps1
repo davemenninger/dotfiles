5028368aafac3905ba6f19fb90b23f4d8070b6dc
Dave Menninger <dave.menninger@cincinnati-oh.gov>
2025-03-07 14:40:10 -0500

    add pwsh theme

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -5,5 +5,8 @@ echo "installing pwsh modules..."
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module -Name posh-git -Repository PSGallery
 
+echo "shell theme..."
+oh-my-posh init pwsh --config 'C:\Users\dmenninger\AppData\Local\Programs\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
+
 echo "installing WSL"
 wsl --install -d Ubuntu-24.04
