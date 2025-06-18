d4adc9858750a98968e5e8f066c342a8682bcf7e
Dave Menninger <dave.menninger@cincinnati-oh.gov>
2025-06-18 16:09:26 -0400

    add blink.nvim

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+local vim = vim
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -142,9 +144,129 @@ require("lazy").setup({
       dependencies = { "nvim-lua/plenary.nvim" },
     },
     {
-      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.4',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+      'saghen/blink.cmp',
+      -- optional: provides snippets for the snippet source
+      dependencies = { 'rafamadriz/friendly-snippets' },
+
+      -- use a release tag to download pre-built binaries
+      version = '1.*',
+      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+      -- build = 'cargo build --release',
+      -- If you use nix, you can build from source using latest nightly rust with:
+      -- build = 'nix run .#build-plugin',
+
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = 'default' },
+
+        appearance = {
+          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono'
+        },
+
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false } },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+      },
+      opts_extend = { "sources.default" }
+    },
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = { 'saghen/blink.cmp' },
+
+      -- example using `opts` for defining servers
+      -- opts = {
+      -- servers = {
+      -- lua_ls = {}
+      -- }
+      -- },
+      -- config = function(_, opts)
+      -- local lspconfig = require('lspconfig')
+      -- for server, config in pairs(opts.servers) do
+      -- -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- -- `opts[server].capabilities, if you've defined it
+      -- config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      -- lspconfig[server].setup(config)
+      -- end
+      -- end
+
+      -- example calling setup directly for each LSP
+      config = function()
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        local lspconfig = require('lspconfig')
+
+        lspconfig['lua_ls'].setup({ capabilities = capabilities })
+      end
+    },
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
+
 
     --- General
     -- use 'ctrlpvim/ctrlp.vim'
@@ -165,7 +287,6 @@ require("lazy").setup({
 })
 
 -- don't remember what this is for
-local vim = vim
 
 require('starry').setup()
 require("telescope").setup()
