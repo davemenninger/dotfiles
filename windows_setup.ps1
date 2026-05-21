1db66b59bff4a0cc2cf190480d919fe07774de31
Dave Menninger <dave.menninger@cincinnati-oh.gov>
2026-05-21 16:52:33 -0400

    remove oh-my-posh

--- windows_setup.ps1
+++ windows_setup.ps1
@@ -2,12 +2,14 @@ echo "installing winget stuff..."
 winget import winget.json
 
 echo "installing pwsh modules..."
-Install-Module -Name Terminal-Icons -Repository PSGallery
-Install-Module -Name posh-git -Repository PSGallery
+# Install-Module -Name Terminal-Icons -Repository PSGallery
+# Install-Module -Name posh-git -Repository PSGallery
+# Install-Module -Name PsReadLine -Repository PSGallery
 
 echo "shell theme..."
-oh-my-posh init pwsh --config 'C:\Users\dmenninger\AppData\Local\Programs\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
-oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\dmenninger\AppData\Local\Programs\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\onehalf.minimal.omp.json' | Invoke-Expression
+# oh-my-posh init powershell --config 'onehalf.minimal' | Invoke-Expression
 
 echo "installing WSL"
 wsl --install -d Ubuntu-24.04
